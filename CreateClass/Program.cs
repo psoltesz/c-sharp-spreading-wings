f539ab9b6d6ecbe3a6ce5da09cc9562ca0cf3457
psoltesz <peter.soltesz92@gmail.com>
2017-06-11 14:03:41 +0200

    Add files via upload

--- CreateClass/Program.cs
+++ CreateClass/Program.cs
@@ -10,31 +10,56 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person();
-            person1.Setup("Jani", "1992.04.02.");
+            Person person1 = new Person("Jani", "1992.04.02.");
+            Employee employee1 = new Employee("Dénes", "1990.04.05.", "programmer", 300000);
             Console.WriteLine(person1.ToString());
+            Console.WriteLine(employee1.ToString());
             Console.ReadKey();
         }
     }
 
     class Person
     {
-        public String name;
-        public String birthdate;
-        Gender gender;
+        public String name, birthdate;
+        public Gender gender;
 
-        enum Gender {male, female};
+        public Person(string name, string birthdate)
+        {
+            this.name = name;
+            this.birthdate = birthdate;
+            gender = Gender.male;
+        }
+        public enum Gender {male, female};
 
         public override string ToString()
         {
             return "Person details: " + name + " (gender: " + gender + "), born at " + birthdate;
         }
 
-        public void Setup(string name, string birthdate)
+    }
+
+    class Employee : Person
+    {
+        
+        String profession;
+        int salary;
+        int roomNumber;
+
+        public Employee(string name, string birthdate, string profession, int salary) : base(name, birthdate)
         {
-            this.name = name;
-            this.birthdate = birthdate;
-            this.gender = Gender.male;
+            this.profession = profession;
+            this.salary = salary;
+            this.roomNumber = Room.roomNumber;
         }
+
+        public override string ToString()
+        {
+            return "Person details: " + name + " (gender: " + gender + "), born at " + birthdate + " - profession: " + profession + ", salary: " + salary + " - room number: " + roomNumber;
+        }
+    }
+
+    class Room
+    {
+        public static int roomNumber = 42;
     }
 }
